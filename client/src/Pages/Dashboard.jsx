0f8b34a1fbeb4bf6ef4a7db67c5f5f38838a87b9
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-30 23:38:04 +0530

    Complete Sidebar
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../Components/DashSideBar";
+import DashProfile from "../Components/DashProfile";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* SIDEBAR */}
+
+      <div className="md:w-56">
+        <DashSideBar />
+      </div>
+      {/* PROFILE AND ETC */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
